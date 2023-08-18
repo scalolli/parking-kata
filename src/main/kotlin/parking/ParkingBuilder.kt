d0a2b54a87445609dfce93d64bdefc1f779dbcea
Basavaraj Kalloli <scalolli@users.noreply.github.com>
2023-08-18 15:52:17 +0100

    introduce the concept of pedestrian exits

--- src/main/kotlin/parking/ParkingBuilder.kt
+++ src/main/kotlin/parking/ParkingBuilder.kt
@@ -1,17 +1,25 @@
 package parking
 
+import kotlin.math.pow
 
-class Parking(val squareSize: Int) {
-    fun getAvailableBays(): Int = squareSize * 4
+
+class Parking(private val squareSize: Int, private val pedestrianExits: List<Int>) {
+    fun getAvailableBays(): Int = squareSize.toDouble().pow(2.toDouble()).toInt() - (pedestrianExits.size)
 }
 
 class ParkingBuilder {
     private var squareSize: Int = 0
+    private val pedestrianExits = mutableListOf<Int>()
 
     fun withSquareSize(squareSize: Int): ParkingBuilder {
         this.squareSize = squareSize
         return this
     }
 
-    fun build() = Parking(squareSize)
+    fun withPedestrianExit(pedestrianExitBayNumber: Int): ParkingBuilder {
+        pedestrianExits.add(pedestrianExitBayNumber)
+        return this
+    }
+
+    fun build() = Parking(squareSize, pedestrianExits)
 }
--- src/test/kotlin/parking/ParkingBuilderTest.kt
+++ src/test/kotlin/parking/ParkingBuilderTest.kt
@@ -7,7 +7,18 @@ class ParkingBuilderTest {
 
     @Test
     fun testBuildBasicParking() {
-        val parking = ParkingBuilder().withSquareSize(4).build()
+        val parking = ParkingBuilder()
+            .withSquareSize(4)
+            .build()
         assertEquals(16, parking.getAvailableBays())
+    }
+
+    @Test
+    fun testBuildParkingWithPedestrianExit() {
+        val parking = ParkingBuilder()
+            .withSquareSize(3)
+            .withPedestrianExit(5)
+            .build()
+        assertEquals(8, parking.getAvailableBays())
     }
 }
