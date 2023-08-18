9826ab3ba01abf1c72bcaa9e7aeb40b3a156da6e
scalolli <scalolli@users.noreply.github.com>
2023-08-18 23:34:41 +0100

    add another test with both pedestrian and disabled slots

--- src/test/kotlin/parking/ParkingBuilderTest.kt
+++ src/test/kotlin/parking/ParkingBuilderTest.kt
@@ -1,33 +1,38 @@
 package parking
 
-import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import org.junit.jupiter.api.Test
 
 class ParkingBuilderTest {
 
-    @Test
-    fun testBuildBasicParking() {
-        val parking = ParkingBuilder()
-            .withSquareSize(4)
-            .build()
-        assertEquals(16, parking.getAvailableBays())
-    }
+  @Test
+  fun testBuildBasicParking() {
+    val parking = ParkingBuilder().withSquareSize(4).build()
+    assertEquals(16, parking.getAvailableBays())
+  }
 
-    @Test
-    fun testBuildParkingWithPedestrianExit() {
-        val parking = ParkingBuilder()
-            .withSquareSize(3)
-            .withPedestrianExit(5)
-            .build()
-        assertEquals(8, parking.getAvailableBays())
-    }
+  @Test
+  fun testBuildParkingWithPedestrianExit() {
+    val parking = ParkingBuilder().withSquareSize(3).withPedestrianExit(5).build()
+    assertEquals(8, parking.getAvailableBays())
+  }
 
-    @Test
-    fun testBuildParkingWithDisabledSlot() {
-        val parking = ParkingBuilder()
-            .withSquareSize(2)
-            .withDisabledBay(2)
-            .build()
-        assertEquals(4, parking.getAvailableBays())
-    }
+  @Test
+  fun testBuildParkingWithDisabledSlot() {
+    val parking = ParkingBuilder().withSquareSize(2).withDisabledBay(2).build()
+    assertEquals(4, parking.getAvailableBays())
+  }
+
+  @Test
+  fun testBuildParkingWithPedestrianExitsAndDisabledSlots() {
+    val parking =
+      ParkingBuilder()
+        .withSquareSize(10)
+        .withPedestrianExit(8)
+        .withPedestrianExit(42)
+        .withPedestrianExit(85)
+        .withDisabledBay(2)
+        .build()
+    assertEquals(97, parking.getAvailableBays())
+  }
 }
