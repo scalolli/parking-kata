51e8d07e7209bec6eee10ae348e3c57d64a8c424
Basavaraj Kalloli <scalolli@users.noreply.github.com>
2023-09-05 16:04:30 +0100

    refactoring to use smart casts

--- src/main/kotlin/parking/Parking.kt
+++ src/main/kotlin/parking/Parking.kt
@@ -10,9 +10,9 @@ class Parking(
 ) {
     sealed class Bay(open val bayNumber: Int)
     sealed class ParkingBay(override val bayNumber: Int, open var car: Char?) : Bay(bayNumber)
-    class DisabledParkingBay(override val bayNumber: Int, override var car: Char?) : ParkingBay(bayNumber, car)
-    class RegularParkingBay(override val bayNumber: Int, override var car: Char?) : ParkingBay(bayNumber, car)
-    class Exit(override val bayNumber: Int) : Bay(bayNumber)
+    private class DisabledParkingBay(override val bayNumber: Int, override var car: Char?) : ParkingBay(bayNumber, car)
+    private class RegularParkingBay(override val bayNumber: Int, override var car: Char?) : ParkingBay(bayNumber, car)
+    private class Exit(override val bayNumber: Int) : Bay(bayNumber)
 
     private val parkingBays = mutableListOf<Bay>()
 
@@ -52,70 +52,43 @@ class Parking(
         }
     }
 
-    private fun getEmptyParkingBays(): List<Bay> {
-        return parkingBays.filter {
-            when (it) {
-                is ParkingBay -> it.car == null
-                else -> false
-            }
-        }
+    private fun getEmptyParkingBays(): List<ParkingBay> {
+        return parkingBays.filter { it is ParkingBay && it.car == null }.map { it as ParkingBay }
     }
 
-    private fun getEmptyDisabledParkingBays(): List<Bay> {
-        return parkingBays.filter {
-            when (it) {
-                is DisabledParkingBay -> it.car == null
-                else -> false
-            }
-        }
+    private fun getEmptyDisabledParkingBays(): List<ParkingBay> {
+        return parkingBays.filter { it is DisabledParkingBay && it.car == null }.map { it as ParkingBay }
     }
 
-    private fun parkCar(emptyBays: List<Bay>, car: Char): Int {
+    private fun parkCar(emptyBays: List<ParkingBay>, car: Char): Int {
         val suitableBaysOrderedByClosetToExit =
             pedestrianExits
                 .flatMap { exit -> emptyBays.map { bay -> abs(bay.bayNumber - exit) to bay } }
                 .sortedBy { it.first }
         val bay = suitableBaysOrderedByClosetToExit.firstOrNull()?.second
-        return bay?.let {
-            return when (it) {
-                is ParkingBay -> {
-                    it.car = car
-                    it.bayNumber
-                }
-                else -> -1
-            }
-        } ?: -1
+        bay?.car = car
+        return bay?.bayNumber ?: -1
     }
 
     fun unParkCar(bayNumber: Int): Boolean {
-        return parkingBays.find {
-            when (it) {
-                is ParkingBay ->
-                    if (it.bayNumber == bayNumber) {
-                        it.car = null
-                        true
-                    } else false
-
-                else -> false
-            }
-        } != null
+        val foundBay = parkingBays.find { it is ParkingBay && it.bayNumber == bayNumber } as? ParkingBay
+        foundBay?.car = null
+        return foundBay != null
     }
 
     override fun toString(): String {
-        val stringBuffer = StringBuffer()
-
-        parkingBays.withIndex().forEach { (index, bay) ->
-            when (bay) {
-                is RegularParkingBay -> bay.car?.let { stringBuffer.append(it) } ?: stringBuffer.append("U")
-                is DisabledParkingBay -> bay.car?.let { stringBuffer.append(it) } ?: stringBuffer.append("@")
-                is Exit -> stringBuffer.append("=")
+        return parkingBays.mapIndexed { index, bay ->
+            val stringPart = when (bay) {
+                is RegularParkingBay -> bay.car?.toString() ?: "U"
+                is DisabledParkingBay -> bay.car?.toString() ?: "@"
+                is Exit -> "="
             }
 
-            if (index != 0 && index != (parkingBays.size - 1) && (index + 1) % squareSize == 0)
-                stringBuffer.append("\n")
-        }
-
-        return stringBuffer.toString()
+            val separator = if (index != 0 && index != (parkingBays.size - 1) && (index + 1) % squareSize == 0)
+                "\n"
+            else ""
+            stringPart + separator
+        }.joinToString(separator = "")
     }
 }
 
