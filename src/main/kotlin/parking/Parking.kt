68e8fd6b7209f7b5f6a2055030df928ee3f89229
Basavaraj Kalloli <scalolli@users.noreply.github.com>
2023-09-05 13:09:37 +0100

    refactored the function parkCar for readability

--- src/main/kotlin/parking/Parking.kt
+++ src/main/kotlin/parking/Parking.kt
@@ -8,6 +8,12 @@ class Parking(
     private val pedestrianExits: List<Int>,
     private val disabledBays: List<Int>
 ) {
+    sealed class Bay(open val bayNumber: Int)
+    sealed class ParkingBay(override val bayNumber: Int, open var car: Char?) : Bay(bayNumber)
+    class DisabledParkingBay(override val bayNumber: Int, override var car: Char?) : ParkingBay(bayNumber, car)
+    class RegularParkingBay(override val bayNumber: Int, override var car: Char?) : ParkingBay(bayNumber, car)
+    class Exit(override val bayNumber: Int) : Bay(bayNumber)
+
     private val parkingBays = mutableListOf<Bay>()
 
     init {
@@ -21,12 +27,13 @@ class Parking(
         }
     }
 
-    fun getAvailableBays(): Int = parkingBays.filter {
-        when (it) {
-            is ParkingBay -> it.car == null
-            else -> false
-        }
-    }.size
+    fun getAvailableBays(): Int =
+        parkingBays.filter {
+            when (it) {
+                is ParkingBay -> it.car == null
+                else -> false
+            }
+        }.size
 
     fun parkCar(car: Char): Int {
         return when (car) {
@@ -64,30 +71,20 @@ class Parking(
     }
 
     private fun parkCar(emptyBays: List<Bay>, car: Char): Int {
-        val bays =
+        val suitableBaysOrderedByClosetToExit =
             pedestrianExits
-                .flatMap { exit ->
-                    emptyBays.map { bay -> abs(bay.bayNumber - exit) to bay }.sortedBy { it.first }
-                }
+                .flatMap { exit -> emptyBays.map { bay -> abs(bay.bayNumber - exit) to bay } }
                 .sortedBy { it.first }
-        val selectedBayNumber = bays.first().second.bayNumber
-        val selectedBay = parkingBays.find { it.bayNumber == selectedBayNumber }
-        selectedBay?.let {
-            when (it) {
-                is RegularParkingBay -> {
+        val bay = suitableBaysOrderedByClosetToExit.firstOrNull()?.second
+        return bay?.let {
+            return when (it) {
+                is ParkingBay -> {
                     it.car = car
-                    true
+                    it.bayNumber
                 }
-
-                is DisabledParkingBay -> {
-                    it.car = 'D'
-                    true
-                }
-
-                else -> null
+                else -> -1
             }
-        }
-        return selectedBayNumber
+        } ?: -1
     }
 
     fun unParkCar(bayNumber: Int): Boolean {
@@ -109,14 +106,9 @@ class Parking(
 
         parkingBays.withIndex().forEach { (index, bay) ->
             when (bay) {
-                is RegularParkingBay ->
-                    bay.car?.let { stringBuffer.append(it) } ?: stringBuffer.append("U")
-
-                is DisabledParkingBay ->
-                    bay.car?.let { stringBuffer.append(it) } ?: stringBuffer.append("@")
-
-                is Exit ->
-                    stringBuffer.append("=")
+                is RegularParkingBay -> bay.car?.let { stringBuffer.append(it) } ?: stringBuffer.append("U")
+                is DisabledParkingBay -> bay.car?.let { stringBuffer.append(it) } ?: stringBuffer.append("@")
+                is Exit -> stringBuffer.append("=")
             }
 
             if (index != 0 && index != (parkingBays.size - 1) && (index + 1) % squareSize == 0)
@@ -125,10 +117,5 @@ class Parking(
 
         return stringBuffer.toString()
     }
-
-    sealed class Bay(open val bayNumber: Int)
-    sealed class ParkingBay(override val bayNumber: Int, open var car: Char?) : Bay(bayNumber)
-    class DisabledParkingBay(override val bayNumber: Int, override var car: Char?) : ParkingBay(bayNumber, car)
-    class RegularParkingBay(override val bayNumber: Int, override var car: Char?) : ParkingBay(bayNumber, car)
-    class Exit(override val bayNumber: Int) : Bay(bayNumber)
 }
+
